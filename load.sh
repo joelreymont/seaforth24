8e8b96b6c769f4b4aefa7c550996c7429d1a1635
Joel Reymont <joelr1@gmail.com>
2009-02-01 09:53:43 +0000

    copy sym file to the debugging host and pause before running driver

--- load.sh
+++ load.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-sudo cp -R build/Debug/SEAforth24.kext /tmp
-sudo kextload /tmp/SEAforth24.kext
-sudo killall -HUP kextd
+cp -R build/Debug/SEAforth24.kext /tmp
+kextload -ls /tmp /tmp/SEAforth24.kext
+mv /tmp/com.wagerlabs.driver.SEAforth24.sym /tmp/SEAforth24.sym
+scp /tmp/SEAforth24.sym mothership.local:/tmp/
+echo "Pausing, hit RETURN to continue...
+read
+kextload -m SEAforth24.kext
+killall -HUP kextd
