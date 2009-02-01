83f89c39eb3b023f8ff149574e14871548ae0c61
Joel Reymont <joelr1@gmail.com>
2009-02-01 11:30:33 +0000

    minor

--- load.sh
+++ load.sh
@@ -3,8 +3,10 @@
 cp -R build/Debug/SEAforth24.kext /tmp
 kextload -ls /tmp /tmp/SEAforth24.kext
 mv /tmp/com.wagerlabs.driver.SEAforth24.sym /tmp/SEAforth24.sym
-scp /tmp/SEAforth24.sym mothership.local:/tmp/
-echo "Pausing, hit RETURN to continue...
+echo "Copying symbol file to debugging host..."
+scp /tmp/SEAforth24.sym joelr@mothership.local:/tmp/
+echo "Pausing, hit RETURN to continue..."
 read
-kextload -m SEAforth24.kext
+echo "Starting driver and re-enumerating..."
+kextload -m /tmp/SEAforth24.kext
 killall -HUP kextd
