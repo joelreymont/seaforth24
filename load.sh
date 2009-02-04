50d1ac417355a5be8299d38876b0ab79bf34770c
Joel Reymont <joelr1@gmail.com>
2009-02-04 11:20:41 +0000

    two kinds of driver loading, regular and for kernel debugging

--- load.sh
+++ load.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
 cp -R build/Debug/SEAforth24.kext /tmp
-kextload -ls /tmp /tmp/SEAforth24.kext
-mv /tmp/com.wagerlabs.driver.SEAforth24.sym /tmp/SEAforth24.sym
-echo "Copying symbol file to debugging host..."
-scp /tmp/SEAforth24.sym joelr@mothership.local:/tmp/
-echo "Pausing, hit RETURN to continue..."
-read
-echo "Starting driver and re-enumerating..."
-kextload -m /tmp/SEAforth24.kext
+kextload /tmp /tmp/SEAforth24.kext
 killall -HUP kextd
+
